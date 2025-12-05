384fa175bcc8a2d1422881ad8f19b37bf4f16e89
Rafael Godlewski <154514341+RafaelGodlewski@users.noreply.github.com>
2025-12-05 21:33:20 +0100

    chore: Update license phrasing from "MIT Licence" to "MIT License" across all source files

--- src/Sanity.Linq/CommonTypes/SanityAssetMetadata.cs
+++ src/Sanity.Linq/CommonTypes/SanityAssetMetadata.cs
@@ -3,14 +3,14 @@
 // This file is part of Sanity LINQ (https://github.com/oslofjord/sanity-linq).
 
 //  Sanity LINQ is free software: you can redistribute it and/or modify
-//  it under the terms of the MIT Licence.
+//  it under the terms of the MIT License.
 
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//  MIT Licence for more details.
+//  MIT License for more details.
 
-//  You should have received a copy of the MIT Licence
+//  You should have received a copy of the MIT License
 //  along with this program.
 
 namespace Sanity.Linq.CommonTypes;
